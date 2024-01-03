2a4d404297915011c21cc52ec1c0158383d855c4
babyporpor01 <152370444+babyporpor01@users.noreply.github.com>
2024-01-03 15:51:27 +0700

    work 4
    work 4

---
+++ post.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard KakKak </h1>
+    <hr>
+    <div style="text-align: center;">
+        <?php 
+            echo "ต้องการดูกระทู้หมายเลข $_GET[id]" ;
+        ?>
+    </div>
+    <table style = "border : 2px solid black; width : 40%;" align="center">
+        <tr><td colspan="2" style = "background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr> 
+        <tr><td><textarea rows = "10" cols="70"></textarea></td></tr>
+        <tr><td colspan = "2" align="center"><input type = "submit" value = "ส่งข้อความ"></td></tr>
+    </table>
+    <br>
+        <div style="text-align: center;"><a href="index.html"; >กลับไปหน้าหลัก</a></div>
+</body>
+</html>
