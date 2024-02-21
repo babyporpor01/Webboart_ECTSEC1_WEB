1f7eaa3b4953779b9acfca6d1d15c5bfefb02256
babyporpor01 <152370444+babyporpor01@users.noreply.github.com>
2024-02-21 15:42:20 +0700

    update webbord by bootstrap
    update file index.php login.php  verify.php  and create nav.php

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
-<?php 
-    session_start();
-    if(isset($_SESSION['id'])){
-      header("location:index.php");
-      die();
-  }
+<?php
+session_start();
+if(isset($_SESSION['id'])){
+    header("location:index.php");
+    die();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,19 +11,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src = "bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-    <h1 style = "text-align: center;">Webboard KakKak</h1>
-    <hr>
-    <form action="verify.php" method="post">
-    <table style = "border : 2px solid black; width : 40%;" align="center">
-        <tr><td colspan="2" style = "background-color: #6CD2FE;">เข้าสู่ระบบ</td></tr>
-        <tr><td>Login</td><td><input type = "text" name = "login" size = "50" required></td></tr>
-        <tr><td>Password</td><td><input type = "password" name = "password" size = "50" required></td></tr>
-        <tr><td colspan = "2" align="center"><input type = "submit" value = "Login"></td></tr>
-    </table>
-    <br>
-    <p style = "text-align: center;">ถ้ายังไม่ได้เป็นสมาชิก <a href = "register.php" >กรุณาสมัครสมาชิก</a></p>
-    </form>
+<div class="container-lg">
+    <h1 style ="text-align: center;" class = "mt-3">Webboard KakKak</h1>
+    <?php include "nav.php" ?>
+    <div class = "row mt-2">
+        <div class = "col-lg-4 col-md-3 col-sm-2 col-1"></div>
+        <div class = "col-lg-4 col-md-6 col-sm-8 col-10">
+            <?php 
+                if(isset($_SESSION['error'])){
+                    echo "<div class='alert alert-danger'>ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</div>";
+                    unset($_SESSION['error']);
+                }
+            ?>
+            <div class="card bg-light text-dark">
+                <div class="card-header">เข้าสู่ระบบ</div>
+                <div class="card-body">
+                <form action="verify.php" method="post">
+                    <div class="form-group">
+                        <label for="login" class = "form-label">Login : </label>
+                        <input type="text" name = "login" id = "login" class = "form-control">
+                    </div>
+                    <div class="from-group mt-2">
+                     <label for="pwd">Password</label>
+                    <input type="password" name = "password" id = "pwd" class="form-control">
+
+                    <div class="d-flex justify-content-center mt-3  ">
+                        <button type="submit" class="btn btn-secondary btn-sm me-2">Login</button>
+                        <button type="reset" class="btn btn-danger btn-sm">Reset</button> 
+                    </div>
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+        <div class = "col-lg-4 col-md-3 col-sm-2 col-1"></div>
+        <div class="from-group mt-2">
+        <p style = "text-align: center;">ถ้ายังไม่ได้เป็นสมาชิก <a href = "register.php" >กรุณาสมัครสมาชิก</a></p>
+        </div>
+    </div>
+</div>
 </body>
 </html>
